7f34a5f195d87ba214662a57c7b6fc0c59f6253f
Edison Johan Bernal <edisonjbernal@gmail.com>
2022-06-08 13:11:11 -0500

    Bonus adicional del ejercicio 2

--- descuentos.js
+++ descuentos.js
@@ -12,3 +12,35 @@ function onClickButtonPriceDiscount(){
     const resultP=document.getElementById("ResultP");
     resultP.innerText="El precio con descuento son: $"+precioConDescuento;
 }
+
+
+const coupons = [
+    {
+        name: "JuanDC_es_Batman",
+        discount: 15,
+    },
+    {
+        name: "pero_no_le_digas_a_nadie",
+        discount: 30,
+    },
+    {
+        name: "es_un_secreto",
+        discount: 25,
+    },
+];
+
+const isCouponValueValid = function (coupon) {
+    return coupon.name === couponValue;
+};
+
+const userCoupon = coupons.find(isCouponValueValid); 
+
+// las dos lineas anteriores son igualea a:
+const userCoupon2 = coupons.find(function (coupon) {
+    return coupon.name === couponValue;
+}); 
+
+
+if (!userCoupon) {
+    alert("El cupón " + couponValue + "no es válido");
+}
